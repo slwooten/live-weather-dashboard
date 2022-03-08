3ce61d1618d1bc331326ebb4245a6c4a30b98823
Seth Wooten <slwooten14@gmail.com>
2022-03-08 17:16:21 -0500

    appended info from geo api fetch to html, added one call api for daily forecast, console logged it

--- assets/script.js
+++ assets/script.js
@@ -2,34 +2,54 @@ var apiKey = 'ca3a4afc9bcd223f595f24041515a78d'
 // var searchInputEl = document.querySelector('#search-input');
 var cityContainerEl = document.querySelector('#city-container');
 var searchBtn = document.querySelector('#search-btn');
+var searchInput = document.getElementById('search-input');
 
 
-// variable for fetching the geo api //
-var getGeoApi = function() {
-    var searchInput = document.getElementById('search-input');
-    var geoUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput.value + "&appid=" + apiKey;
-
+// function for fetching the geo api //
+var getApis = function() {
+    
+    var geoUrl = "https://api.openweathermap.org/data/2.5/weather/?&q=" + searchInput.value + "&appid=" + apiKey + "&units=imperial";
+   
     fetch(geoUrl) 
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            for (var i = 0; i < data.length; i++) {
+            console.log('heres the data', data);
+            
                 var cityName = document.createElement('h3');
-                var temp = document.createElement('<p>');  
-                var wind = document.createElement('<p>');  
-                var humidity = document.createElement('<p>');  
-                var uvIndex = document.createElement('<p>'); 
-                
+                var temp = document.createElement('p');  
+                var wind = document.createElement('p');  
+                var humidity = document.createElement('p');  
+                var uvIndex = document.createElement('p'); 
+                var latitude = data.coord.lat;
+                var long = data.coord.lon;
 
-                cityName.innerHTML(data[i].name);
+                cityName.textContent = data.name;
+                temp.textContent = 'Temp: ' + data.main.temp + '°F';
+                wind.textContent = 'Wind: ' + data.wind.speed + ' MPH';
+                humidity.textContent = 'Humidity: ' + data.main.humidity + '%';
 
-                cityContainerEl.append(cityName);
-            }
+                cityContainerEl.appendChild(cityName);
+                cityContainerEl.appendChild(temp);
+                cityContainerEl.appendChild(wind);
+                cityContainerEl.appendChild(humidity);
+
+                // url for the onecall api, gets daily weather using lat and lon from geo fetch //
+                var oneCallApi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + long + "&appid=" + apiKey;
+            
+                fetch(oneCallApi)
+                    .then(function (response) {
+                        return response.json();
+                    })
+                    .then(function (data) {
+                        console.log(data);
+                    })
         })
+    
+        
 }
 
-searchBtn.addEventListener('click', getGeoApi);
+searchBtn.addEventListener('click', getApis);
 
 
